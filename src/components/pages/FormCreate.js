c1841e4610a7309ef67811193d37b445dbafb78c
Kiowa Andueza <kiowaandueza@imac-5.intechpartner.com>
2022-10-27 16:08:49 +0100

    Initialize project using Create React App

---
+++ src/components/pages/FormCreate.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormCreate = () => {
+    return(
+        <div>
+            <h1>Pagina formulario</h1>
+        </div>
+    )
+}
+
+export default FormCreate
---
+++ src/components/pages/Table.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Table = () => {
+    return(
+        <div>
+            <h1>Pagina inicio</h1>
+        </div>
+    )
+}
+
+export default Table
